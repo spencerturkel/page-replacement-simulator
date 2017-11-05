8441c2b6166df9d279412aa6757433ebb3cd5d30
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-11-05 14:21:54 -0500

    Reading input

--- Projectos/main.cpp
+++ Projectos/main.cpp
@@ -11,27 +11,18 @@ struct file_input_retriever : public input_retriever
 	std::vector<int> retrieve() const override
 	{
 		auto result = std::vector<int>{};
-		auto inputstream = std::ifstream{"inputfile.dat"};
+		auto inputstream = std::ifstream{"inputfile", std::ios_base::binary};
 
 		std::string line;
-		std::cout << "Reading line\n";
-		while (getline(inputstream, line))
+		while (getline(inputstream >> std::ws, line))
 		{
-			if (line == "stop")
-			{
-				break;
-			}
 			int current_int;
-//			std::stringstream ss{line};
-			std::cout << "Line: " << line << '\n';
-			inputstream >> current_int;
-			result.push_back(current_int);
+			std::stringstream ss{line};
 
-//			while (ss >> current_int)
-//			{
-//				std::cout << "current_int: " << current_int << '\n';
-//				result.push_back(current_int);
-//			}
+			while (ss >> current_int)
+			{
+				result.push_back(current_int);
+			}
 		}
 
 		return result;
