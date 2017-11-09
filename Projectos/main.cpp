832b60ef96bc54afd9900e3ce873d49f2a682b31
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-11-09 15:47:28 -0500

    Add cxxopts

--- Projectos/main.cpp
+++ Projectos/main.cpp
@@ -3,6 +3,8 @@
 #include <numeric>
 #include <string>
 
+#include <cxxopts.hpp>
+
 #include "file_input_retriever.h"
 #include "iterative_replacement_algorithm.h"
 #include "fifo_iterative_replacement_algorithm.h"
@@ -64,17 +66,60 @@ auto make_retriever() -> std::unique_ptr<input_retriever>
 	return std::make_unique<file_input_retriever>();
 }
 
-auto main() -> int
+auto make_options(int& argc, char**& argv) -> cxxopts::Options
 {
+	using namespace cxxopts;
+
+	auto options = Options{"PageReplacementSimulator", "OS Group Project"};
+
+	options.add_options()
+		("f,file", "An input file, where each line is formatted as specified in the --input option", value<std::string>())
+		("input", "A space-seperated string of positive integers representing a memory trace", value<std::vector<int>>())
+		("p,pages", "Number of pages in simulated page table", value<int>()->default_value("4"))
+		("s,show-steps", "Show each state of the page table when running an algorithm")
+		("a,alg,algorithm", "Choose the algorithm to run. Must be one of 'all', 'fifo', 'lfu', 'lru', 'mfu', or 'opt'",
+		 value<std::string>()->default_value("all"));
+
+	options.parse_positional("input");
+
+	options.parse(argc, argv);
+	return options;
+}
+
+auto main(int argc, char** argv) -> int
+{
+	const auto options_parser = make_options(argc, argv);
 	const auto all_inputs = make_retriever()->retrieve();
+	const auto alg = options_parser["alg"].as<std::string>();
+	const auto pages = options_parser["pages"].as<int>();
+	const auto show_steps = options_parser["show-steps"].as<bool>();
 
 	for (auto&& input : all_inputs)
 	{
-		report_iterative_algorithm<fifo_iterative_replacement_algorithm>(4, input, false);
-		report_iterative_algorithm<lfu_iterative_replacement_algorithm>(4, input, false);
-		report_iterative_algorithm<lru_iterative_replacement_algorithm>(4, input, false);
-		report_iterative_algorithm<mfu_iterative_replacement_algorithm>(4, input, false);
-		report_iterative_algorithm<optimal_iterative_replacement_algorithm>(4, input, false);
+		if (alg == "all" || alg == "fifo")
+		{
+			report_iterative_algorithm<fifo_iterative_replacement_algorithm>(pages, input, show_steps);
+		}
+
+		if (alg == "all" || alg == "lfu")
+		{
+			report_iterative_algorithm<lfu_iterative_replacement_algorithm>(pages, input, show_steps);
+		}
+
+		if (alg == "all" || alg == "lru")
+		{
+			report_iterative_algorithm<lru_iterative_replacement_algorithm>(pages, input, show_steps);
+		}
+
+		if (alg == "all" || alg == "mfu")
+		{
+			report_iterative_algorithm<mfu_iterative_replacement_algorithm>(pages, input, show_steps);
+		}
+
+		if (alg == "all" || alg == "opt")
+		{
+			report_iterative_algorithm<optimal_iterative_replacement_algorithm>(pages, input, show_steps);
+		}
 	}
 
 	std::cout << "Enter anything to exit...\n";
