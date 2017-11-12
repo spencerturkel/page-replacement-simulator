f888f8d09179177d4993b1e335796f2a422ea6e3
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-11-12 15:01:24 -0500

    Modularize MFU

--- include/mfu_iterative_replacement_algorithm.h
+++ include/mfu_iterative_replacement_algorithm.h
@@ -7,9 +7,3 @@ struct mfu_iterative_replacement_algorithm : frequency_iterative_replacement_alg
 	explicit mfu_iterative_replacement_algorithm(int page_table_size);
 };
 
-
-mfu_iterative_replacement_algorithm::
-mfu_iterative_replacement_algorithm(const int page_table_size): frequency_iterative_replacement_algorithm(
-	"Most Frequently Used", page_table_size, page_used_less_than)
-{
-}
