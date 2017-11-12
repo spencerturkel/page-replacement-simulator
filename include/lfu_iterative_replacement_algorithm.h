256406f10c0472d204ebb8bc1eb36e3e8bdae694
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-11-12 14:52:43 -0500

    Seperate defs/decls for LFU

--- include/lfu_iterative_replacement_algorithm.h
+++ include/lfu_iterative_replacement_algorithm.h
@@ -2,17 +2,7 @@
 
 #include "frequency_iterative_algorithm.h"
 
-auto page_used_greater_than(const page& left, const page& right) -> bool
-{
-	return left.used > right.used;
-}
-
 struct lfu_iterative_replacement_algorithm : frequency_iterative_replacement_algorithm
 {
 	explicit lfu_iterative_replacement_algorithm(const int page_table_size);
 };
-
-lfu_iterative_replacement_algorithm::lfu_iterative_replacement_algorithm(const int page_table_size)
-	: frequency_iterative_replacement_algorithm("Least Frequently Used", page_table_size, page_used_greater_than)
-{
-}
